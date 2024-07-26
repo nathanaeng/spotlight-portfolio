5df340d8f0918fefd162babe2c3633d8515d9589
Nathan Eng <nathaneng89@gmail.com>
2024-07-26 16:18:01 -0700

    Slight syling improvements and re-add result check for empty data

--- client/src/components/results/Results.jsx
+++ client/src/components/results/Results.jsx
@@ -1,9 +1,10 @@
 import Result from './Result.jsx';
 import styles from './Results.module.css';
+import searchbarStyles from '../search-bar/Searchbar.module.css';
 
 const Results = ({ data }) => {
-  // if (input && input.value !== "" && data.length > 0) {
-  if (data.length > 0) {
+  const input = document.querySelector(`.${searchbarStyles['search-input']}`);
+  if (input && input.value !== '' && data.length > 0) {
     return (
       <div className={styles['results-container']}>
         {Object.keys(data).map((e, idx) => (<Result key={idx} data={data[e]} />))}
